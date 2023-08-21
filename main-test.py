ffae4108555132f2b02c9a08725d892c7ed3e05a
ziad amr shalaby <zezoamr@yandex.com>
2023-08-21 22:24:29 +0300

    initial commit

---
+++ main-test.py
@@ -0,0 +1,5 @@
+from main import Add
+
+def test_Add():
+    assert Add(2,3) == 5
+    print("Add Function works correctly")
---
+++ main.py
@@ -0,0 +1,9 @@
+def Add(a, b):
+        return a + b
+        
+def SayHello():
+        print("hello world")
+
+if __name__ == '__main__':
+        SayHello()
+        print(Add(2, 3))
